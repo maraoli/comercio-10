360b181aeadbce1b71e62ee841641b34e80cf56f
Mara <mara.15oliveira@outlook.com>
2020-12-02 23:01:26 -0300

    alt in home

--- comercio-10/src/pages/homepage/Homepage.js
+++ comercio-10/src/pages/homepage/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageDefault from '../../components/pageDefault/PageDefault';
 import bkg_store from '../../assets/background_store.jpg';
+import {Link} from 'react-router-dom';
 import './Homepage.css';
 
 export default function Homepage(){
@@ -16,11 +17,30 @@ export default function Homepage(){
 
                 <section className="sessao sessao_sobre" id="sessao_sobre">
                     <h1>O que é a Comércio 10?</h1>
-                    <p>Aqui na Comércio 10 você pode tirar esse sonho do papel, pois além de contarmos com uma assesoria especializada para colocar a sua empresa nas nuvens temos um time especialista em consultoria para você não perder o financeiro de controle quando suas vendas aumentarem.</p>
+                    <div className="sobre_conteudo">
+                        <p>Aqui na Comércio 10 você pode tirar esse sonho do papel, pois além de contarmos com uma assesoria especializada para colocar a sua empresa nas nuvens temos um time especialista em consultoria para você não perder o financeiro de controle quando suas vendas aumentarem.</p>
+                        {/* <img src={} alt="nossa empresa" /> */}
+                    </div>
                 </section>
 
                 <section className="sessao sessao_servicos" id="sessao_servicos">
                     <h1>Que tal conhecer um de nossos serviços?</h1>
+                    <div className="servicos">
+
+                        <Link to="/produtos">
+                            <div className="servico servico_1">
+                                <p>Quero ter um <span>site</span> para o meu negócio.</p>
+                            </div>
+                        </Link>
+
+                        <Link to="/produtos">
+                            <div className="servico servico_2">
+                                <p>Quero solicitar uma <span>consultoria</span> para o meu negócio.</p>
+                            </div>
+                        </Link>
+
+                    </div>
+                    
                 </section> 
 
                 <section className="sessao sessao_contatos" id="sessao_contatos">
